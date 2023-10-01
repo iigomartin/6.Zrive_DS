e5d2d652a70fde5f27cbc67130dff4367c3b0cd4
iigomartin <inigo.martin98@gmail.com>
2023-10-01 17:30:56 +0200

    make lint, tests working, make tests

--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,29 +1,56 @@
 """ This is a dummy example to show how to import code from src/ for testing"""
 
-#from src.module_1.module_1_meteo_api import main
+
 import pandas as pd
-
-def test_main():
-    
-    test__calculate_statistics()
-    
-    raise NotImplementedError
-
 from src.module_1.module_1_meteo_api import calculate_statistics
 
+
 def test__calculate_statistics():
-    testing=pd.DataFrame(data={"temperature_2m_mean":[1,2,3,4],"precipitation_sum":[5,6,7,8],
-                               "soil_moisture_0_to_10cm_mean":[9,10,11,12] })
-    AVG_temp=(1+2+3+4)/4
-    SD_temp=(((1-AVG_temp)^2+(2-AVG_temp)^2+(3-AVG_temp)^2+(4-AVG_temp)^2)/4)^0.5
-    AVG_precip=(5+6+7+8)/4
-    SD_precip=(((5-AVG_precip)^2+(6-AVG_precip)^2+(7-AVG_precip)^2+(8-AVG_precip)^2)/4)^0.5
-    AVG_soil=(9+10+11+12)/4
-    SD_soil=(((9-AVG_soil)^2+(10-AVG_soil)^2+(11-AVG_soil)^2+(12-AVG_soil)^2)/4)^0.5
-    
-    correct={"AVG_temperature_2m_mean":AVG_temp,"SD_temperature_2m_mean":SD_temp,"AVG_precipitation_sum":AVG_precip,
-            "SD_precipitation_sum":SD_precip,"AVG_soil_moisture_0_to_10cm_mean":  AVG_soil,
-            "SD_soil_moisture_0_to_10cm_mean":SD_soil}
-    
+    testing = pd.DataFrame(
+        data={
+            "temperature_2m_mean": [1, 2, 3, 4],
+            "precipitation_sum": [5, 6, 7, 8],
+            "soil_moisture_0_to_10cm_mean": [9, 10, 11, 12],
+        }
+    )
+    AVG_temp = (1 + 2 + 3 + 4) / 4
+    SD_temp = (
+        (
+            (1 - AVG_temp) ** 2
+            + (2 - AVG_temp) ** 2
+            + (3 - AVG_temp) ** 2
+            + (4 - AVG_temp) ** 2
+        )
+        / 3
+    ) ** 0.5
+    AVG_precip = (5 + 6 + 7 + 8) / 4
+    SD_precip = (
+        (
+            (5 - AVG_precip) ** 2
+            + (6 - AVG_precip) ** 2
+            + (7 - AVG_precip) ** 2
+            + (8 - AVG_precip) ** 2
+        )
+        / 3
+    ) ** 0.5
+    AVG_soil = (9 + 10 + 11 + 12) / 4
+    SD_soil = (
+        (
+            (9 - AVG_soil) ** 2
+            + (10 - AVG_soil) ** 2
+            + (11 - AVG_soil) ** 2
+            + (12 - AVG_soil) ** 2
+        )
+        / 3
+    ) ** 0.5
+
+    correct = {
+        "AVG_temperature_2m_mean": AVG_temp,
+        "SD_temperature_2m_mean": SD_temp,
+        "AVG_precipitation_sum": AVG_precip,
+        "SD_precipitation_sum": SD_precip,
+        "AVG_soil_moisture_0_to_10cm_mean": AVG_soil,
+        "SD_soil_moisture_0_to_10cm_mean": SD_soil,
+    }
+
     assert correct == calculate_statistics(testing)
-    
