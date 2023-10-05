bcf0f43eaf7f750f8bf29ee0dd9be6e4556ef2c8
iigomartin <inigo.martin98@gmail.com>
2023-10-05 15:31:21 +0200

    check adding gitignore download script

---
+++ src/module_2/download_data.py
@@ -0,0 +1,12 @@
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    print("This is a test, should not be uploaded")
+    
+
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    main()
